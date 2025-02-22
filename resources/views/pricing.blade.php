ebd59f45b4c915f4b17f2dc29ed4d2006c046d2f
sachintha999 <sachinthadil53383@gmail.com>
2025-02-22 22:01:26 +0530

    update

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -13,12 +13,20 @@
                 <div class="card-body d-flex flex-column">
                     <!-- Card Content -->
                     <div>
-                        <h5 class="text-center pricing-title">One time Payment</h5>
+                        <h5 class="text-center pricing-title">One Time Pay</h5>
                         <p class="text-center pricing-title-description">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
                         </p>
                         <h1 class="text-center price-plan">$FREE</h1>
                         <div class="text-center">
+                            <div>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
+                                        <path fill="#f1935d" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/>
+                                    </svg>
+                                </span>
+                                <span class="pricing-text">Lorem ipsum dolor sit amet, consectetur</span>
+                            </div>
                             <div>
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
@@ -50,7 +58,7 @@
                 <div class="card-body d-flex flex-column">
                     <!-- Card Content -->
                     <div>
-                        <h5 class="text-center pricing-title">One time Payment</h5>
+                        <h5 class="text-center pricing-title">Annual Pay </h5>
                         <p class="text-center pricing-title-description">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
                         </p>
@@ -73,6 +81,14 @@
                                 <span class="pricing-text">Lorem ipsum dolor sit amet, consectetur</span>
                             </div>
 
+                            <div>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
+                                        <path fill="#f1935d" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/>
+                                    </svg>
+                                </span>
+                                <span class="pricing-text">Lorem ipsum dolor sit amet, consectetur</span>
+                            </div>
                             <div>
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
@@ -105,7 +121,7 @@
                 <div class="card-body d-flex flex-column">
                     <!-- Card Content -->
                     <div>
-                        <h5 class="text-center pricing-title">One time Payment</h5>
+                        <h5 class="text-center pricing-title">Annual Pay </h5>
                         <p class="text-center pricing-title-description">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
                         </p>
